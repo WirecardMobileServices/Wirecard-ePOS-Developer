c897bfa429fd2138bf561879d045bdc9d9a2bb30
radoslav.danko <radoslav.danko@wirecard.com>
2018-03-05 11:16:44 +0100

    iOS Ref doc updated

--- iosref/search/variables_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['wdeposversionnumber',['WDePosVersionNumber',['../_w_de_pos_8h.html#adc10b9fdcab2dee4938489b5b0994806',1,'WDePos.h']]],
-  ['wdeposversionstring',['WDePosVersionString',['../_w_de_pos_8h.html#ab68c72d7710b3a8f4946f5c68735bfa5',1,'WDePos.h']]]
-];
